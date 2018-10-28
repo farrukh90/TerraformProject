c18d25e08c774baa506c459d93e9638e1de8d26c
farrukh90 <farrukhsadykov@gmail.com>
2018-10-28 22:22:43 +0000

    fixed all

--- instance.tf
+++ instance.tf
@@ -1,14 +1,6 @@
-resource "aws_instance" "instance1" {
-  ami                    = "ami-0c21ae4a3bd190229"
-  instance_type          = "t2.micro"
-  key_name               = "project1"
-  subnet_id              = "${aws_subnet.artemis-public.id}"
-  vpc_security_group_ids = ["${aws_security_group.artemis.id}"]
-  user_data = "${file("apache.sh")}"
-  associate_public_ip_address = "true"
-}
+# Web
 resource "aws_instance" "instance2" {
-  ami                    = "ami-0c21ae4a3bd190229"
+  ami                    = "${lookup(var.amis, var.region)}"
   instance_type          = "t2.micro"
   key_name               = "project1"
   subnet_id              = "${aws_subnet.artemis-public.id}"
@@ -16,3 +8,14 @@ resource "aws_instance" "instance2" {
   user_data = "${file("apache.sh")}"
   associate_public_ip_address = "true"
 }
+
+# DB Host
+resource "aws_instance" "dbhost" {
+  ami                    = "${lookup(var.amis, var.region)}"
+  instance_type          = "t2.micro"
+  key_name               = "project1"
+  subnet_id              = "${aws_subnet.artemis-private.id}"
+  vpc_security_group_ids = ["${aws_security_group.artemis.id}"]
+  user_data = "${file("db.sh")}"
+}
+
--- output.tf
+++ output.tf
@@ -1,9 +1,3 @@
-output "public_ip" {
-	value = "${aws_instance.instance1.public_ip}"
-}
-output "ID" { 
-       value = "${aws_instance.instance1.id}"
-}
 
 output "ID2" {
        value = "${aws_instance.instance2.id}"
