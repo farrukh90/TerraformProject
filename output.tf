14cb8c5a9787d572d12f1b852cc4d0dc6ba175c4
farrukh90 <farrukhsadykov@gmail.com>
2018-10-28 21:56:07 +0000

    working

--- output.tf
+++ output.tf
@@ -4,3 +4,9 @@ output "public_ip" {
 output "ID" { 
        value = "${aws_instance.instance1.id}"
 }
+output "public_ip" {
+	value = "${aws_instance.instance2.public_ip}"
+}
+output "ID" { 
+       value = "${aws_instance.instance2.id}"
+}
